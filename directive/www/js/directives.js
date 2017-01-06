707ab89cc3cdca66e13f6bb88c513c75ef828f59
Masayoshi Nakamura <masayang@Masayoshis-MacBook-Pro.local>
2017-01-06 00:08:03 -0800

    OK. it's done

--- directive/www/js/directives.js
+++ directive/www/js/directives.js
@@ -3,7 +3,16 @@ angular.module('starter.directives', [])
 .directive('helloWorld', function() {
     return {
         restrict: 'AE',
-        replace: 'true',
-        template: '<h1>Hello World!</h1>'
+        replace: true,
+        templateUrl: 'templates/hello-world.html'
+    }
+})
+
+.directive('linkTest', function() {
+    return {
+        scope: true,
+        restrict: 'AE',
+        replace: true,
+        templateUrl: 'templates/link-test.html'
     }
 })
